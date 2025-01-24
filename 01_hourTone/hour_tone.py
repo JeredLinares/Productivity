e318db2c4f187bc47395d42a1f1f69b970475fc4
JD Linares <JeredL@gmail.com>
2025-01-24 17:11:52 -0500

    added time zone

--- 01_hourTone/hour_tone.py
+++ 01_hourTone/hour_tone.py
@@ -5,51 +5,59 @@ Make a sound on the hour
 
 JD Linares
 2025 01 22
+2025 01 24
+
+Add to crontab: 
+# Note TIMEZONE
+# m h  dom mon dow   command
+  0 7-22  *  *    *     /bin/python /home/diego/projects/Productivity/01_hourTone/hour_tone.py
+
 '''
 
+import  datetime
 from gpiozero import PWMOutputDevice
 import time
 
+
+my_tz = datetime.timezone(datetime.timedelta(hours=-5))
+my_time = datetime.datetime.now(my_tz)
+#print(str(my_time))
+my_hour = my_time.hour%12      # convert to 12 hour time
+
+my_ten = my_hour // 10          # 10's place
+my_one = my_hour % 10           #  1's place
+
 hour_tone = PWMOutputDevice(pin=12)
-hour_tone.on()
-time.sleep(4)
-hour_tone.off()
 
-'''
-# m h  dom mon dow   command
-  0 7-22  *  *    *     /bin/python /home/diego/projects/Productivity/01_hourTone/hour_tone.py
-'''
-
-
-
-
-'''
-c = 261
-d = 293
-e = 329
-f = 349
-g = 392
-
-hour_tone.frequency = c
-hour_tone.on()
-time.sleep(1)
-hour_tone.off()
-hour_tone.frequency = d
-hour_tone.on()
-time.sleep(1)
-hour_tone.off()
-hour_tone.frequency = e
-hour_tone.on()
-time.sleep(1)
-hour_tone.off()
-hour_tone.frequency = c
-hour_tone.on()
-time.sleep(1)
-hour_tone.off()
+if my_ten:
+    hour_tone.on()
+    time.sleep(4)
+    hour_tone.off()
+    time.sleep(1)
+else:
+    hour_tone.on()
+    time.sleep(.1)
+    hour_tone.off()
+    time.sleep(.05)
+    hour_tone.on()
+    time.sleep(.1)
+    hour_tone.off()
+    time.sleep(.05)
+    hour_tone.on()
+    time.sleep(.1)
+    hour_tone.off()
+    time.sleep(.05)
+    hour_tone.on()
+    time.sleep(.1)
+    hour_tone.off()
+    time.sleep(1)
 
 
+for i in range(my_one):
+    hour_tone.on()
+    time.sleep(1)
+    hour_tone.off()
+    time.sleep(1)
 
-
-'''
 
 
